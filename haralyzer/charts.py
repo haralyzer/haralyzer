c0045c64b2ab2dc88364b31d5241a13bc1813c9c
Justin Crown <jcrown@mediatemple.net>
2015-03-01 22:26:22 -0800

    Adding graph types and scale

--- haralyzer/charts.py
+++ haralyzer/charts.py
@@ -39,12 +39,14 @@ def make_chart(chart_type=None, har_files=None):
             all_pages.extend(har_parser.pages)
 
     if chart_type == 'load_time_by_content_pie':
-        make_load_time_by_content_pie(all_pages)
+        load_time_by_content_pie(all_pages)
     elif chart_type == 'load_time_by_content_bar':
-        make_load_time_by_content_bar(all_pages)
+        load_time_by_content_bar(all_pages)
+    elif chart_type == 'total_load_time':
+        total_load_time(all_pages)
 
 
-def make_load_time_by_content_pie(pages):
+def load_time_by_content_pie(pages):
     """
     Renders a pie chart for load time by content type, showing the percentage
     of the load time of each asset type in relation to the total load time.
@@ -72,7 +74,7 @@ def make_load_time_by_content_pie(pages):
         pie_chart.render_to_file('{0}.svg'.format(tld))
 
 
-def make_load_time_by_content_bar(pages):
+def load_time_by_content_bar(pages):
     """
     Renders a bar chart for load time by content type. Unlike the pie chart,
     this shows actual load time in seconds.
@@ -84,15 +86,83 @@ def make_load_time_by_content_bar(pages):
     bar_chart = pygal.Bar()
     bar_chart.x_labels = map(str, pages[0].parser.content_types)
     bar_chart.title = 'Load time by content for {0}'.format(
-        'testing')
+        '"testing" in seconds')
 
+    timings = dict()
+    all_times = []
     for page in pages:
         tld = urlparse(page.url).netloc
-        timings = []
+        # This will end up being a big ``list`` of dict. Each key is the name
+        # of the page, and the value is a ``list`` of the content type timings
+        page_timings = []
         for c_type in page.parser.content_types:
-            load_time = getattr(page, '{0}_load_time'.format(c_type))
-            timings.append(load_time)
+            page_timings.append(getattr(page, '{0}_load_time'.format(c_type)))
 
-        bar_chart.add(page.url, timings)
+        # timings.append({'name': page.url, 'timings': page_timings})
+        timings[tld] = page_timings
+        all_times.extend(page_timings)
+
+        # Get multiplier for all values
+        for page_id, timeset in timings.iteritems():
+            all_times.extend(timeset)
+
+    scale, multiplier = _create_multiplier(all_times, val_type='time')
+
+    bar_chart.title = 'Load time by content in {0}'.format(scale)
+
+    for page_id, timeset in timings.iteritems():
+        bar_chart.add(page_id, [t * multiplier for t in timeset])
 
     bar_chart.render_to_file('{0}.svg'.format(tld))
+
+
+def total_load_time(pages):
+    """
+    Renders a bar chart of the total load time for each page.
+    """
+    bar_chart = pygal.Bar()
+    all_load_times = []
+    load_times = dict()
+    for page in pages:
+        all_load_times.append(page.total_load_time)
+        tld = urlparse(page.url).netloc
+        load_times[tld] = page.total_load_time
+
+    scale, multiplier = _create_multiplier(all_load_times, val_type='time')
+
+    bar_chart.title = 'Load time by content in {0}'.format(scale)
+
+    for page_id, timing in load_times.iteritems():
+        bar_chart.add(page_id, [timing * multiplier])
+
+    bar_chart.render_to_file('{0}.svg'.format(tld))
+
+
+def _create_multiplier(vals, val_type=''):
+    """
+    Takes an array of values and returns the proper multiplier for
+    scaling charts.
+
+    :param vals: a ``list`` of ``int`` of the vals to examine
+    :param measurement: ``str`` of value type, valid values are:
+
+        * time - this assumes that all input is in ms
+        * size = this assumes that all input is in bytes
+
+    :returns: ``typle`` where [0] is a ``str`` of unit type, and [1] is
+    an``int`` of the multiplier to scale by
+    """
+    if val_type == 'time':
+        # If something is less than two seconds... keep it at ms
+        if any(v <= 2000 for v in vals):
+            return ('ms', 1)
+        # For now, we will only scale to seconds at the greatest
+        else:
+            return ('seconds', 1000)
+    elif val_type == 'size':
+        # If something is less than 1 MB, scale to KB
+        if any(v <= 1000000 for v in vals):
+            return ('KB', 1024)
+        # As of right now, we only scale up to MB
+        else:
+            return ('MB', 2048)
