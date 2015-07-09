fa500f8d8ebfd2d76bb7cbbd351e1e730519e0ad
Justin Crown <jcrown@mediatemple.net>
2015-07-09 09:36:37 -0700

    Bumpversion, allow nightly build to fail

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ readme = open('README.rst').read()
 
 setup(
     name='haralyzer',
-    version='1.2.1',
+    version='1.2.2',
     description='A python framework for getting useful stuff out of HAR files',
     long_description=readme,
     author='Justin Crown',
