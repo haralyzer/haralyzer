fcfd7b226ea970d889cbaa50e7abedb6c7cd60e4
Justin Crown <jcrown@mediatemple.net>
2016-04-14 07:19:56 -0700

    bumpversion

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ readme = open('README.rst').read()
 
 setup(
         name='haralyzer',
-        version='1.4.9',
+        version='1.4.10',
         description='A python framework for getting useful stuff out of HAR files',
         long_description=readme,
         author='Justin Crown',
