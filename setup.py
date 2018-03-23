4808eec1d486523d299a6c22f358b4f3b2181659
Justin Crown <justincrown1@gmail.com>
2018-03-23 16:01:15 -0700

    no longer supporting 2.6

--- setup.py
+++ setup.py
@@ -42,9 +42,7 @@ setup(
             'Intended Audience :: Developers',
             'License :: OSI Approved :: MIT License',
             'Natural Language :: English',
-            'Programming Language :: Python :: 2',
             'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 2.6',
             'Programming Language :: Python :: 2.7',
         ],
 )
